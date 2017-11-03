1ee03319e8d3d10d583911b63419181f860550fb
Carlos Hernandez <learnercys@gmail.com>
2017-11-03 00:06:02 -0600

    Basic structure
    This is a basic structure for https://nodeschool.io/guatemala/ using GatsbyJS

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <Link to="/instructions/">Go to Instructions</Link>
+  </div>
+)
+
+export default IndexPage
